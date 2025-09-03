33a32827f1a313ef51261064b9c7fbd13334f93a
jdeiby06 <jdeibylopez@estudiante.uniajc.edu.co>
2025-09-02 20:19:08 -0500

    Se agrego los objetos libro 1 y 2

--- Main.java
+++ Main.java
@@ -6,11 +6,19 @@ public class Main {
     private  int numEjemplares;
     private  int numPrestados;
 
+    
+
+    
+    
+    
+     
     public static void main(String[] args) {
         
         Libro libro1 = new Libro("En agosto nos vemos", "Gabriel Garcia Marquez", 4, 2);
 
         
+        System.out.println("Datos del libro 1:");
+        libro1.Imprimir();
         Libro libro2 = new Libro();
 
         
@@ -27,14 +35,76 @@ public class Main {
 
         System.out.print("Ingrese el número de ejemplares prestados del libro 2: ");
         libro2.setNumPrestados(sc.nextInt());
-
         
-        System.out.println("\nDatos del libro 1:");
-        libro1.Imprimir();
+        
 
-        System.out.println("\nDatos del libro 2:");
+        System.out.println("Datos del libro 2:");
         libro2.Imprimir();
 
         sc.close();
     }
+    
+
+
+    public boolean prestamo() {
+        if (numPrestados < numEjemplares) {
+            numPrestados=numPrestados + 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean devolucion() {
+        if (numPrestados > 0) {
+            numPrestados=numPrestados - 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+   
+    public void  Imprimir() {
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Numero de ejemplares: " + numEjemplares);
+        System.out.println("Numero de ejemplares prestados: " + numPrestados);
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumEjemplares() {
+        return numEjemplares;
+    }
+
+    public void setNumEjemplares(int numEjemplares) {
+        this.numEjemplares = numEjemplares;
+    }
+
+    public int getNumPrestados() {
+        return numPrestados;
+    }
+
+    public void setNumPrestados(int numPrestados) {
+        this.numPrestados = numPrestados;
+
+    
+    
+    
+
+}
 }
